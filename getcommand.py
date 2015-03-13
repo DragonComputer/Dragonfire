086fb4b1c4ca082137d315aff8af11de86b26ae3
Mehmet Mert Yıldıran mehmetmertyildiran@gmail.com <>
2015-03-13 17:29:58 +0200

    Gender prefix configuration functions added. IfElifElse added.

--- getcommand.py
+++ getcommand.py
@@ -21,8 +21,8 @@ def command(speech_object):
 		line = speech_object.readline()
 		if(line.startswith("sentence1: ")):
 			com = line[15:-6]
-			if (previous_command == com):
-				continue
+			#if (previous_command == com):
+			#	continue
 			print com
 
                 	Config = ConfigParser.ConfigParser()
@@ -38,49 +38,64 @@ def command(speech_object):
 					2 : "What is your orders?"
 				}
 				userin.say(words_dragonfire[randint(0,2)])
-			if (com == "ENOUGH" or com == "OKAY"):
+			elif (com == "ENOUGH" or com == "OKAY"):
 				tts_kill()
-			if (com == "WHO AM I"):
+			elif (com == "WHO AM I" or com == "WHAT IS MY NAME"):
                                 tts_kill()
 				user_full_name = os.popen("getent passwd $LOGNAME | cut -d: -f5 | cut -d, -f1").read()
 				user_full_name = user_full_name[:-1]
                                 userin = Data(["echo"], user_full_name)
-                                userin.say("You are " + user_full_name + ",sir")
+                                userin.say("Your name is " + user_full_name + "," + user_prefix + ".")
 				userin.interact(0)
                                 previous_command = com
-			if (com == "WHAT IS YOUR NAME"):
+			elif (com == "I'M A WOMAN" or com == "I'M A GIRL" or com == "I'M A LADY"):
+				tts_kill()
+				cfgfile = open("config.ini",'w')
+				Config.set("BasicUserData","Prefix","My Lady")
+				Config.write(cfgfile)
+				cfgfile.close()
+				userin = Data([" "]," ")
+				userin.say("Pardon, My Lady.")
+                        elif (com == "I'M A MAN" or com == "I'M A BOY"):
+                                tts_kill()
+                                cfgfile = open("config.ini",'w')
+                                Config.set("BasicUserData","Prefix","Sir")
+                                Config.write(cfgfile)
+                                cfgfile.close()
+                                userin = Data([" "]," ")
+                                userin.say("Pardon, Sir.")
+			elif (com == "WHAT IS YOUR NAME"):
 				tts_kill()
 				userin = Data([" "]," ")
 				userin.say("My name is Dragon Fire.")
 				previous_command = com
-			if (com == "WHAT IS YOUR GENDER"):
+			elif (com == "WHAT IS YOUR GENDER"):
 				tts_kill()
                                 userin = Data([" "]," ")
-                                userin.say("I have a female voice but I don't have a gender identity. I'm a computer program " + user_prefix + ".")
+                                userin.say("I have a female voice but I don't have a gender identity. I'm a computer program, " + user_prefix + ".")
                                 previous_command = com
-        		if (com == "OPEN FILE MANAGER"):
+        		elif (com == "OPEN FILE MANAGER"):
 				tts_kill()
 				userin = Data(["pantheon-files"],"File Manager")
 				userin.say("File Manager")
 				userin.interact(0)
 				previous_command = com
-			if (com == "OPEN WEB BROWSER"):
+			elif (com == "OPEN WEB BROWSER"):
 				tts_kill()
-				userin= Data(["sensible-browser"],"Web Browser")
+				userin = Data(["sensible-browser"],"Web Browser")
 				userin.say("Web Browser")
 				userin.interact(0)
 				previous_command = com
-			if (com == "SHUT DOWN THE COMPUTER"):
+			elif (com == "SHUT DOWN THE COMPUTER"):
 				tts_kill()
-                                userin= Data(["sudo","poweroff"],"Shutting down")
+                                userin = Data(["sudo","poweroff"],"Shutting down")
                                 userin.say("Shutting down")
 				userin.interact(3)
                                 previous_command = com
-			if (com.startswith("SEARCH FOR")):
+			elif (com.startswith("SEARCH FOR")):
 				tts_kill()
 				userin = Data(["sensible-browser","http://en.wikipedia.org/wiki/"+com[11:].lower()],com[11:])
 				userin.interact(0)
-				
 				try:
 					wikipage = wikipedia.page(com[11:].lower())
 					wikicontent = "".join([i if ord(i) < 128 else ' ' for i in wikipage.content])
@@ -88,7 +103,12 @@ def command(speech_object):
             				previous_command = com
 				except:
 					pass
-			
+			else:
+				tts_kill()
+                                userin = Data(["echo"],com + " ?")
+                                userin.say("Unrecognized command.")
+                                userin.interact(0)
+                                previous_command = com
 
 def tts_kill():
 	call(["pkill", "audsp"])
