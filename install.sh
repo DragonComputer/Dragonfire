6a173c4e2d2462015fbb09c56c09a68fe9b1fbc8
Ismail Korkmaz <ismail.korkmaz@bil.omu.edu.tr>
2016-05-07 00:45:53 +0300

    Important dependency fix related with PyAudio and FLAC.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-apt-get -y install julius festival festlex-cmu python-xlib
+apt-get -y install julius festival festlex-cmu python-xlib portaudio19-dev python-all-dev flac
 cd /usr/share/festival/voices/english/ && wget -c http://www.speech.cs.cmu.edu/cmu_arctic/packed/cmu_us_clb_arctic-0.95-release.tar.bz2 && tar jxf cmu_us_clb_arctic-0.95-release.tar.bz2 && ln -s cmu_us_clb_arctic cmu_us_clb_arctic_clunits && cp /etc/festival.scm /etc/festival.scm.backup && chmod o+w /etc/festival.scm && echo "(set! voice_default 'voice_cmu_us_clb_arctic_clunits)" >> /etc/festival.scm
