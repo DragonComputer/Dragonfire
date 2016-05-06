bdc4ca04b9b6a91a5dce7e827ad84cb27acd8974
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-06 03:05:45 +0300

    install.sh bash script added again.

--- README.rst
+++ README.rst
@@ -19,7 +19,7 @@ Dragonfire does following tasks for each separate command, respectively:
 Version
 ~~~~~~~
 
-0.8.3
+0.8.4
 
 Installation
 ~~~~~~~~~~~~
--- setup.py
+++ setup.py
@@ -11,58 +11,12 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 import os
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-from setuptools.command.egg_info import egg_info
-#import getpass
 
 here = path.abspath(path.dirname(__file__))
-#username = getpass.getuser()
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 	long_description = f.read()
-
-class PostDevelopCommand(develop):
-	"""Post-installation for development mode."""
-	def run(self):
-		import nltk
-
-		#nltk.download("punkt")
-		#nltk.download("averaged_perceptron_tagger")
-		#nltk.download("tagsets")
-		nltk.download("brown")
-		nltk.download("names")
-
-		os.system("apt-get -y install julius festival festlex-cmu python-xlib")
-		os.system("cd /usr/share/festival/voices/english/ && wget -c http://www.speech.cs.cmu.edu/cmu_arctic/packed/cmu_us_clb_arctic-0.95-release.tar.bz2 && tar jxf cmu_us_clb_arctic-0.95-release.tar.bz2 && ln -s cmu_us_clb_arctic cmu_us_clb_arctic_clunits && cp /etc/festival.scm /etc/festival.scm.backup && chmod o+w /etc/festival.scm && echo \"(set! voice_default 'voice_cmu_us_clb_arctic_clunits)\" >> /etc/festival.scm")
-		#os.system("cd /usr/share/ && wget http://ml.nec-labs.com/senna/senna-v3.0.tgz && tar zxvf senna-v3.0.tgz")
-		#os.system("sudo apt-get -y install openjdk-7-jdk git && cd /home/" + username + "/ && sudo -u " + username + " git clone https://github.com/brmson/yodaqa.git ; cd yodaqa/ && sudo -u " + username + " git pull && sudo -u " + username + " ./gradlew check && sudo -u " + username + " echo | ./gradlew run")
-		#os.system("sudo apt-get -y install openjdk-7-jdk git && cd /var/tmp/ && git clone https://github.com/brmson/yodaqa.git ; cd yodaqa/ && git pull && ./gradlew check && echo | ./gradlew run")
-
-		develop.run(self)
-
-class PostInstallCommand(install):
-	"""Post-installation for installation mode."""
-	def run(self):
-		import nltk
-		nltk.download("brown")
-		nltk.download("names")
-		os.system("apt-get -y install julius festival festlex-cmu python-xlib")
-		os.system("cd /usr/share/festival/voices/english/ && wget -c http://www.speech.cs.cmu.edu/cmu_arctic/packed/cmu_us_clb_arctic-0.95-release.tar.bz2 && tar jxf cmu_us_clb_arctic-0.95-release.tar.bz2 && ln -s cmu_us_clb_arctic cmu_us_clb_arctic_clunits && cp /etc/festival.scm /etc/festival.scm.backup && chmod o+w /etc/festival.scm && echo \"(set! voice_default 'voice_cmu_us_clb_arctic_clunits)\" >> /etc/festival.scm")
-
-		install.run(self)
-
-class PostInstallegg_info(egg_info):
-	"""Post-installation for sdist."""
-	def run(self):
-		import nltk
-		nltk.download("brown")
-		nltk.download("names")
-		os.system("apt-get -y install julius festival festlex-cmu python-xlib")
-		os.system("cd /usr/share/festival/voices/english/ && wget -c http://www.speech.cs.cmu.edu/cmu_arctic/packed/cmu_us_clb_arctic-0.95-release.tar.bz2 && tar jxf cmu_us_clb_arctic-0.95-release.tar.bz2 && ln -s cmu_us_clb_arctic cmu_us_clb_arctic_clunits && cp /etc/festival.scm /etc/festival.scm.backup && chmod o+w /etc/festival.scm && echo \"(set! voice_default 'voice_cmu_us_clb_arctic_clunits)\" >> /etc/festival.scm")
-
-		egg_info.run(self)
 
 setup(
 	name='dragonfire',
@@ -70,7 +24,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.8.3',
+	version='0.8.4',
 
 	description='Dragonfire is an open source virtual assistant project for Ubuntu based Linux distributions',
 	long_description=long_description,
@@ -155,12 +109,6 @@ setup(
 	# http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
 	# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
 	data_files=[],
-
-	cmdclass={
-		'develop': PostDevelopCommand,
-		'install': PostInstallCommand,
-		'egg_info': PostInstallegg_info,
-	},
 
 	# To provide executable scripts, use entry points in preference to the
 	# "scripts" keyword. Entry points provide cross-platform support and allow
