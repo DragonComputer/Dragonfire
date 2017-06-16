661824646aae9b7b5af666e5d2959a78f7ba52ad
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-06-16 14:51:10 +0300

    Start to implement AIML parsing natively

--- dragonfire/learn.py
+++ dragonfire/learn.py
@@ -6,24 +6,45 @@ import sys
 import contextlib
 import cStringIO
 from random import randint
-import uuid
+import collections
+import pkg_resources
+import xml.etree.ElementTree as ET
+import re
 
 class Aiml():
 
-	@staticmethod
-	def respond(address, convo_id, query):
-		url = address + '/chatbot/conversation_start.php'
-		#url = 'http://localhost:4567/q'
-		post_params = {
-			'say' : query,
-			'convo_id' : convo_id
-		}
+	def __init__(self):
+		self.dictionary = collections.OrderedDict()
+		tree = ET.parse(pkg_resources.resource_filename('dragonfire', 'aiml/computers.aiml'))
+		root = tree.getroot()
+		#print collection.toxml()
+		categories = root.findall("category")
+		for category in categories:
+			key = self.patternParser(category.find("pattern"))
+			value = self.templateParser(category.find("template"))
+			self.dictionary[key] = value
 
-		params = urllib.urlencode(post_params)
-		response = urllib2.urlopen(url, params)
-		json_response = json.loads(response.read())
+	def respond(self,com):
+		for key,value in self.dictionary.iteritems():
+			print key
+			matches = re.findall(key, com)
+			print matches
+			if matches:
+				for match in matches:
+					print match
+				return value
 
-		return json_response['botsay']
+	def patternParser(self,this_node):
+	    return this_node.text.replace('*','(.*)')
+
+	def templateParser(self,this_node):
+		i = 0
+		for child in this_node.iter():
+			if child.tag == 'set':
+				this_node.remove(child)
+				this_node.insert(i, 'asdas')
+			i += 1
+		return this_node.text
 
 
 def noanswer(user_prefix):
@@ -49,6 +70,5 @@ def nostderr():
 	sys.stderr = save_stderr
 
 if __name__ == "__main__":
-	convo_id = uuid.uuid4()
-	print Aiml.respond("http://teach.dragon.computer/", convo_id, "Learn the sun is hot")
-	print Aiml.respond("http://teach.dragon.computer/", convo_id, "What is the sun")
+	AimlObj = Aiml()
+	print AimlObj.respond("MY COMPUTER IS AN ASSHOLE")
