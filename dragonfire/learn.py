0b8500bb33f3b69a40a2d6b33e48f93b1c90d067
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-06-18 17:04:36 +0300

    Fix the AIML with Complex Regex Grammar Model

--- dragonfire/learn.py
+++ dragonfire/learn.py
@@ -6,6 +6,8 @@ import collections
 import pkg_resources
 from lxml import etree
 import re
+from tinydb import TinyDB, Query
+from os.path import expanduser
 
 class Aiml():
 
@@ -13,34 +15,57 @@ class Aiml():
 		self.replacements = {"I'M": "YOU", "YOU ARE": "I'M", "YOU": "I'M", "MY ": "YOUR ", "YOUR ": "MY "}
 		self.grammar_model = collections.OrderedDict()
 		self.grammar_model = {
-			"(?P<col1>.*) (?P<col2>IS|ARE|WAS|WERE|WILL BE) (?P<col3>.*)": "(?:WHO|WHERE|WHEN|WHY|WHAT|WHICH|HOW)(?:.*)(?P<col2>IS|ARE|WAS|WERE|WILL BE) (?P<col1>.*)",
-			"(?P<col1>.*) (?P<col2>MEANS?|HAS|HAVE|LIVES?) (?P<col3>.*)": "(?:WHO|WHERE|WHEN|WHY|WHAT|WHICH|HOW)(?:.*)(?:DOES|DO) (?P<col1>.*) (?P<col2>MEAN|HAVE|LIVE)",
-			"(?P<col1>.*) (?P<col2>SAID) (?P<col3>.*)": "(?:WHO|WHERE|WHEN|WHY|WHAT|WHICH|HOW)(?:.*)(?:DID) (?P<col1>.*) (?P<col2>SAY)"
+			"(?P<subject>.*) (?P<verbtense>IS|ARE|WAS|WERE|WILL BE) (?P<clause>.*)": "(?:WHO|WHERE|WHEN|WHY|WHAT|WHICH|HOW)(?:.*)(?P<verbtense>IS|ARE|WAS|WERE|WILL BE) (?P<subject>.*)",
+			"(?P<subject>.*) (?P<verbtense>MEANS?|HAS|HAVE|LIVES?) (?P<clause>.*)": "(?:WHO|WHERE|WHEN|WHY|WHAT|WHICH|HOW)(?:.*)(?:DOES|DO) (?P<subject>.*) (?P<verbtense>MEAN|HAVE|LIVE)",
+			"(?P<subject>.*) (?P<verbtense>SAID) (?P<clause>.*)": "(?:WHO|WHERE|WHEN|WHY|WHAT|WHICH|HOW)(?:.*)(?:DID) (?P<subject>.*) (?P<verbtense>SAY)"
 		}
+		home = expanduser("~")
+		self.db = TinyDB(home + '/.dragonfire.json')
 
 	def respond(self,com):
+		forget = "^FORGET (?:ABOUT )(?P<subject>.*)"
+		capture = re.search(forget, com)
+		if capture:
+			self.db.remove(Query().subject == capture.group('subject'))
+			return "OK, I FORGOT EVERYTHING ABOUT " + capture.group('subject').upper()
+
 		for statement,question in self.grammar_model.iteritems():
 
 			capture = re.search(question, com)
 			if capture:
-				answer = capture.group()
-				return answer.upper()
+				result = self.db.search(Query().subject == capture.group('subject'))
+				if result:
+					for row in result:
+						original_clause = row['clause']
+						for item in result[:]:
+							if row['verbtense'] == item['verbtense'] and original_clause != item['clause']:
+								row['clause'] = row['clause'] + ' AND ' + item['clause']
+								result.remove(item)
+					answer = ''
+					for row in result:
+						if answer:
+							answer += ', '
+						answer += row['subject'] + ' ' + row['verbtense'] + ' ' + row['clause']
+					return self.mirror(answer)
+				else:
+					return None
 
 			capture = re.search(statement, com)
 			if capture:
-				#print capture.group('col2')
-				#print capture.group('col3')
-				#print capture.group('col1')
-				answer = capture.group()
-				answer = answer.upper()
-				for key,value in self.replacements.iteritems():
-					if key in answer:
-						answer = answer.replace(key,value)
-						break
+				if not self.db.search( (Query().subject == capture.group('subject')) & (Query().verbtense == capture.group('verbtense')) & (Query().clause == capture.group('clause')) ):
+					self.db.insert({'subject': capture.group('subject'), 'verbtense': capture.group('verbtense'), 'clause': capture.group('clause')})
+				answer = self.mirror(capture.group())
 				return "OK, I GET IT. " + answer
 
 		return None
 
+	def mirror(self,answer):
+		answer = answer.upper()
+		for key,value in self.replacements.iteritems():
+			if key in answer:
+				answer = answer.replace(key,value)
+				break
+		return answer
 
 def noanswer(user_prefix):
 	words_dragonfire = {
@@ -66,4 +91,6 @@ def nostderr():
 
 if __name__ == "__main__":
 	AimlObj = Aiml()
-	print AimlObj.respond("MY AGE IS 24")
+	print AimlObj.respond("THE SUN IS HOT")
+	print AimlObj.respond("THE SUN IS YELLOW")
+	print AimlObj.respond("WHAT IS THE SUN")
