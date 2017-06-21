02459d4aa8fe92f318d27c13fd8ea48fb387191b
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-06-21 16:42:35 +0300

    Add lxml package to the dependency list

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
 	# your project is installed. For an analysis of "install_requires" vs pip's
 	# requirements files see:
 	# https://packaging.python.org/en/latest/requirements.html
-	install_requires=['wikipedia','PyUserInput','google-api-python-client','SpeechRecognition','nltk','egenix-mx-base','httplib2>=0.9.1','pyaudio','tinydb','youtube_dl'],
+	install_requires=['wikipedia','PyUserInput','google-api-python-client','SpeechRecognition','nltk','egenix-mx-base','httplib2>=0.9.1','pyaudio','tinydb','youtube_dl','lxml'],
 
 	# List additional groups of dependencies here (e.g. development
 	# dependencies). You can install these using the following syntax,
