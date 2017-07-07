f9e9a9ebaeaaf344827cd18fd2308a868ffd808c
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-07-07 03:15:21 +0300

    Integrate the avatar animation into text-to-speech

--- dragonfire/utilities.py
+++ dragonfire/utilities.py
@@ -7,6 +7,10 @@ import time
 import os
 import inspect
 import realhud
+import sys
+import contextlib
+import cStringIO
+from multiprocessing import Pool
 
 DRAGONFIRE_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 FNULL = open(os.devnull, 'w')
@@ -15,7 +19,7 @@ songRunning = False
 class TTA:
 
 	def __init__(self):
-		pass
+		realhud.load_gif(DRAGONFIRE_PATH + "/realhud/animation/avatar.gif")
 
 	def define(self, com="", msg="", sp="False"):
 		self.command = com
@@ -51,18 +55,46 @@ class TTA:
 			else:
 				print "Dragonfire: " + message.upper()
 				print "_______________________________________________________________\n"
-		proc = subprocess.Popen("festival --tts", stdin=subprocess.PIPE, stdout=FNULL, stderr=FNULL, shell=True)
+		tts_proc = subprocess.Popen("festival --tts", stdin=subprocess.PIPE, stdout=FNULL, stderr=FNULL, shell=True)
 		message = "".join([i if ord(i) < 128 else ' ' for i in message])
-		proc.stdin.write(message)
-		proc.stdin.close()
-		#proc.wait()
+		tts_proc.stdin.write(message)
+		tts_proc.stdin.close()
+		#print "TTS process started."
+
+		pool = Pool(processes=1)
+		pool.apply_async(realhud.play_gif, [0.5, True])
+		#print "Avatar process started."
+
+		tts_proc.wait()
+		pool.terminate()
 		#if songRunning == True:
 		#	subprocess.Popen(["rhythmbox-client","--play"])
 
 	def espeak(self,message):
 		subprocess.Popen(["espeak","-v","en-uk-north",message])
 
+	def play_gif_wrapper(opacity,is_click_through):
+		with nostdout():
+			with nostderr():
+				realhud.play_gif(opacity, is_click_through)
+
+
+@contextlib.contextmanager
+def nostdout():
+	save_stdout = sys.stdout
+	sys.stdout = cStringIO.StringIO()
+	yield
+	sys.stdout = save_stdout
+
+@contextlib.contextmanager
+def nostderr():
+	save_stderr = sys.stderr
+	sys.stderr = cStringIO.StringIO()
+	yield
+	sys.stderr = save_stderr
+
+
 if __name__ == "__main__":
 	userin = TTA()
 	userin.define([" "]," ")
-	userin.say("I have a female voice but I don't have a gender identity. I'm a computer program.")
+	userin.say("Hello world!")
