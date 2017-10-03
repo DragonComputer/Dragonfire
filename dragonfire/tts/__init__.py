d77a83b9c524201d4e6e3f74d13c7c58382d25cb
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2017-10-03 18:12:31 +0300

    Add some parameters

--- dragonfire/tts/__init__.py
+++ dragonfire/tts/__init__.py
@@ -1,5 +1,11 @@
-import csv
+__author__ = 'Mehmet Mert Yildiran, mert.yildiran@bil.omu.edu.tr'
 
+import csv # CSV (Comma Separated Values) format is the most common import and export format for spreadsheets and databases
+
+CHUNK = 1024 # Smallest unit of audio. 1024 bytes
+FORMAT = pyaudio.paInt16 # Data format
+CHANNELS = 2 # Number of channels
+RATE = 44100 # Bit Rate of audio stream / Frame Rate
 
 class Synthesizer():
 
@@ -18,6 +24,13 @@ class Synthesizer():
         string = string.replace(',','')
         words = string.split()
 
-        result = ""
+        result = []
         for word in words:
-            print self.word_map[word]
+            if word in self.word_map:
+                result.append(self.word_map[word])
+        return result
+
+
+if __name__ == "__main__":
+    syn = Synthesizer()
+    print syn.string_to_phonemes("I have a female voice but I don't have a gender identity. I'm a computer program, sir.")
