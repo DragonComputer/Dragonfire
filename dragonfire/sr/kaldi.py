94de935026030408c526e6441fe37cffbafbe17c
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2017-10-14 06:53:45 +0300

    Create an online recognizer test for Kaldi GStreamer decoder

--- dragonfire/sr/kaldi.py
+++ dragonfire/sr/kaldi.py
@@ -6,7 +6,14 @@ import thread
 import logging
 from decoder import DecoderPipeline
 import time
+import pyaudio # Provides Python bindings for PortAudio, the cross platform audio API
 
+CHUNK = 8000 # Smallest unit of audio. 1024 bytes
+FORMAT = pyaudio.paInt16 # Data format
+CHANNELS = 1 # Number of channels
+RATE = 16000 # Bit Rate of audio stream / Frame Rate
+THRESHOLD = 1000 # Threshhold value for detecting stimulant
+RECORD_SECONDS = 10
 
 class DecoderPipelineTests(unittest.TestCase):
 
@@ -16,21 +23,21 @@ class DecoderPipelineTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-            # voxforge/tri2b_mmi_b0.05 model:
-            decoder_conf = {"model" : "models/english/final.mdl",
-                            "lda-mat" : "models/english/final.mat",
-                            "word-syms" : "models/english/words.txt",
-                            "fst" : "models/english/HCLG.fst",
-                            "silence-phones" : "6"}
-            cls.decoder_pipeline = DecoderPipeline({"decoder" : decoder_conf})
-            cls.words = []
-            cls.finished = False
+        # voxforge/tri2b_mmi_b0.05 model:
+        decoder_conf = {"model" : "models/english/final.mdl",
+                        "lda-mat" : "models/english/final.mat",
+                        "word-syms" : "models/english/words.txt",
+                        "fst" : "models/english/HCLG.fst",
+                        "silence-phones" : "6"}
+        cls.decoder_pipeline = DecoderPipeline({"decoder" : decoder_conf})
+        cls.words = []
+        cls.finished = False
 
-            cls.decoder_pipeline.set_word_handler(cls.word_getter)
-            cls.decoder_pipeline.set_eos_handler(cls.set_finished, cls.finished)
+        cls.decoder_pipeline.set_word_handler(cls.word_getter)
+        cls.decoder_pipeline.set_eos_handler(cls.set_finished, cls.finished)
 
-            loop = GObject.MainLoop()
-            thread.start_new_thread(loop.run, ())
+        loop = GObject.MainLoop()
+        thread.start_new_thread(loop.run, ())
 
     @classmethod
     def word_getter(cls, word):
@@ -44,6 +51,34 @@ class DecoderPipelineTests(unittest.TestCase):
         self.__class__.words = []
         self.__class__.finished = False
 
+
+
+    def testRecognize(self):
+        self.decoder_pipeline.init_request("recognize", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+        p = pyaudio.PyAudio() # Create a PyAudio session
+        # Create a stream
+        stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=CHUNK)
+
+        data = stream.read(CHUNK) # Get first data frame from the microphone
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK) # Read a new chunk from the stream
+            stream.write(data, CHUNK)
+            #time.sleep(0.25)
+            self.decoder_pipeline.process_data(data)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        self.decoder_pipeline.end_request()
+        while not self.finished:
+            time.sleep(1)
+        print self.words
 
 
     def testWav(self):
