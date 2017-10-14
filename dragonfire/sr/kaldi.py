2a2bfc7f33314b1b2a4dbb81475c5a74aebda3d3
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2017-10-14 07:45:57 +0300

    Make Kaldi speech recognition continuous

--- dragonfire/sr/kaldi.py
+++ dragonfire/sr/kaldi.py
@@ -7,13 +7,15 @@ import logging
 from decoder import DecoderPipeline
 import time
 import pyaudio # Provides Python bindings for PortAudio, the cross platform audio API
+import audioop # Operates on sound fragments consisting of signed integer samples 8, 16 or 32 bits wide, stored in Python strings.
 
 CHUNK = 8000 # Smallest unit of audio. 1024 bytes
 FORMAT = pyaudio.paInt16 # Data format
 CHANNELS = 1 # Number of channels
 RATE = 16000 # Bit Rate of audio stream / Frame Rate
 THRESHOLD = 1000 # Threshhold value for detecting stimulant
-RECORD_SECONDS = 10
+SILENCE_DETECTION = 5 # Wait number of frames to decide whether it fell silent or not
+LISTENING = True
 
 class KaldiRecognizer():
 
@@ -44,14 +46,12 @@ class KaldiRecognizer():
     def set_finished(self, finished):
         self.finished = True
 
-    def setUp(self):
+    def reset(self):
         self.__class__.words = []
         self.__class__.finished = False
 
-
-
     def recognize(self):
-        self.decoder_pipeline.init_request("recognize", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+
         p = pyaudio.PyAudio() # Create a PyAudio session
         # Create a stream
         stream = p.open(format=FORMAT,
@@ -61,21 +61,39 @@ class KaldiRecognizer():
                     output=True,
                     frames_per_buffer=CHUNK)
 
-        data = stream.read(CHUNK) # Get first data frame from the microphone
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK) # Read a new chunk from the stream
-            stream.write(data, CHUNK)
-            #time.sleep(0.25)
-            self.decoder_pipeline.process_data(data)
+        try:
+            data = stream.read(CHUNK) # Get first data frame from the microphone
+            # Loop over the frames of the audio / data chunks
+            while data != '':
+                rms = audioop.rms(data, 2) # Calculate Root Mean Square of current chunk
+                if rms >= THRESHOLD: # If Root Mean Square value is greater than THRESHOLD constant
+                    self.decoder_pipeline.init_request("recognize", "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1")
+                    self.decoder_pipeline.process_data(data)
+                    silence_counter = 0 # Define silence counter
+                    while silence_counter < SILENCE_DETECTION: # While silence counter value less than SILENCE_DETECTION constant
+                        data = stream.read(CHUNK) # Read a new chunk from the stream
+                        if LISTENING: stream.write(data, CHUNK)
+                        self.decoder_pipeline.process_data(data)
 
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+                        rms = audioop.rms(data, 2) # Calculate Root Mean Square of current chunk again
+                        if rms < THRESHOLD: # If Root Mean Square value is less than THRESHOLD constant
+                            silence_counter += 1 # Then increase silence counter
+                        else: # Else
+                            silence_counter = 0 # Assign zero value to silence counter
 
-        self.decoder_pipeline.end_request()
-        while not self.finished:
-            time.sleep(1)
-        print self.words
+                    self.decoder_pipeline.end_request()
+                    while not self.finished:
+                        time.sleep(1)
+                    print self.words
+                    self.reset()
+
+                data = stream.read(CHUNK) # Read a new chunk from the stream
+                if LISTENING: stream.write(data, CHUNK)
+
+        except KeyboardInterrupt:
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
 
 
 
