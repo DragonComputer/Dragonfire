17b7ef745ef9af7aef177fac78d365dea926475c
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2017-10-19 19:08:15 +0300

    Remove unnecessary google-api-python-client dependency

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
 	# your project is installed. For an analysis of "install_requires" vs pip's
 	# requirements files see:
 	# https://packaging.python.org/en/latest/requirements.html
-	install_requires=['wikipedia','PyUserInput','google-api-python-client','tinydb','youtube_dl','spacy','pyowm'],
+	install_requires=['wikipedia','PyUserInput','tinydb','youtube_dl','spacy','pyowm'],
 
 	# List additional groups of dependencies here (e.g. development
 	# dependencies). You can install these using the following syntax,
