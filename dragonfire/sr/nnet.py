977926333b8262498bfb0935ac5cdf305d6e674f
cclauss <cclauss@bluewin.ch>
2017-12-29 01:26:02 +0100

    Reformat the code with autopep8

--- dragonfire/sr/nnet.py
+++ dragonfire/sr/nnet.py
@@ -6,88 +6,105 @@ import theano
 import theano.tensor as T
 
 try:
-    xrange          # Python 2
+    xrange  # Python 2
 except NameError:
     xrange = range  # Python 3
 
 
 class RNN():
+    def __init__(self, nin, n_hidden, nout):
+        rng = np.random.RandomState(1234)
+        self.W_uh = np.asarray(
+            rng.normal(size=(nin, n_hidden), scale=.01, loc=.0),
+            dtype=theano.config.floatX)
+        self.W_hh = np.asarray(
+            rng.normal(size=(n_hidden, n_hidden), scale=.01, loc=.0),
+            dtype=theano.config.floatX)
+        self.W_hy = np.asarray(
+            rng.normal(size=(n_hidden, nout), scale=.01, loc=0.0),
+            dtype=theano.config.floatX)
+        self.b_hh = np.zeros((n_hidden, ), dtype=theano.config.floatX)
+        self.b_hy = np.zeros((nout, ), dtype=theano.config.floatX)
+        self.activ = T.nnet.sigmoid
+        lr = T.scalar()
+        u = T.matrix()
+        t = T.vector()
 
-	def __init__(self, nin, n_hidden, nout):
-		rng = np.random.RandomState(1234)
-		self.W_uh = np.asarray(
-			rng.normal(size=(nin, n_hidden), scale= .01, loc = .0), dtype = theano.config.floatX)
-		self.W_hh = np.asarray(
-			rng.normal(size=(n_hidden, n_hidden), scale=.01, loc = .0), dtype = theano.config.floatX)
-		self.W_hy = np.asarray(
-			rng.normal(size=(n_hidden, nout), scale =.01, loc=0.0), dtype = theano.config.floatX)
-		self.b_hh = np.zeros((n_hidden,), dtype=theano.config.floatX)
-		self.b_hy = np.zeros((nout,), dtype=theano.config.floatX)
-		self.activ = T.nnet.sigmoid
-		lr = T.scalar()
-		u = T.matrix()
-		t = T.vector()
+        self.W_uh = theano.shared(self.W_uh, 'W_uh')
+        self.W_hh = theano.shared(self.W_hh, 'W_hh')
+        self.W_hy = theano.shared(self.W_hy, 'W_hy')
+        self.b_hh = theano.shared(self.b_hh, 'b_hh')
+        self.b_hy = theano.shared(self.b_hy, 'b_hy')
 
-		self.W_uh = theano.shared(self.W_uh, 'W_uh')
-		self.W_hh = theano.shared(self.W_hh, 'W_hh')
-		self.W_hy = theano.shared(self.W_hy, 'W_hy')
-		self.b_hh = theano.shared(self.b_hh, 'b_hh')
-		self.b_hy = theano.shared(self.b_hy, 'b_hy')
+        self.h0_tm1 = theano.shared(
+            np.zeros(n_hidden, dtype=theano.config.floatX))
 
-		self.h0_tm1 = theano.shared(np.zeros(n_hidden, dtype=theano.config.floatX))
+        h, _ = theano.scan(
+            self.recurrent_fn,
+            sequences=u,
+            outputs_info=[self.h0_tm1],
+            non_sequences=[self.W_hh, self.W_uh, self.W_hy, self.b_hh])
 
-		h, _ = theano.scan(self.recurrent_fn, sequences = u,
-					   outputs_info = [self.h0_tm1],
-					   non_sequences = [self.W_hh, self.W_uh, self.W_hy, self.b_hh])
+        y = T.dot(h[-1], self.W_hy) + self.b_hy
+        cost = ((t - y)**2).mean(axis=0).sum()
 
-		y = T.dot(h[-1], self.W_hy) + self.b_hy
-		cost = ((t - y)**2).mean(axis=0).sum()
+        gW_hh, gW_uh, gW_hy,\
+            gb_hh, gb_hy = T.grad(
+                cost, [self.W_hh, self.W_uh, self.W_hy, self.b_hh, self.b_hy])
 
-		gW_hh, gW_uh, gW_hy,\
-		   gb_hh, gb_hy = T.grad(
-			   cost, [self.W_hh, self.W_uh, self.W_hy, self.b_hh, self.b_hy])
+        self.train_step = theano.function(
+            [u, t, lr],
+            cost,
+            on_unused_input='warn',
+            updates=[(self.W_hh, self.W_hh - lr * gW_hh),
+                     (self.W_uh,
+                      self.W_uh - lr * gW_uh), (self.W_hy,
+                                                self.W_hy - lr * gW_hy),
+                     (self.b_hh,
+                      self.b_hh - lr * gb_hh), (self.b_hy,
+                                                self.b_hy - lr * gb_hy)],
+            allow_input_downcast=True)
 
-		self.train_step = theano.function([u, t, lr], cost,
-							on_unused_input='warn',
-							updates=[(self.W_hh, self.W_hh - lr*gW_hh),
-									 (self.W_uh, self.W_uh - lr*gW_uh),
-									 (self.W_hy, self.W_hy - lr*gW_hy),
-									 (self.b_hh, self.b_hh - lr*gb_hh),
-									 (self.b_hy, self.b_hy - lr*gb_hy)],
-							allow_input_downcast=True)
+        self.run = theano.function([u], y)
 
-		self.run = theano.function([u], y)
+    def recurrent_fn(self, u_t, h_tm1, W_hh, W_uh, W_hy, b_hh):
+        h_t = self.activ(T.dot(h_tm1, W_hh) + T.dot(u_t, W_uh) + b_hh)
+        return h_t
 
-	def recurrent_fn(self, u_t, h_tm1, W_hh, W_uh, W_hy, b_hh):
-		h_t = self.activ(T.dot(h_tm1, W_hh) + T.dot(u_t, W_uh) + b_hh)
-		return h_t
+    def dump(self, path, filename='model.npz'):
+        with open(path + filename, "wb") as thefile:
+            np.savez(
+                thefile,
+                W_uh=self.W_uh.get_value(),
+                W_hh=self.W_hh.get_value(),
+                W_hy=self.W_hy.get_value(),
+                b_hh=self.b_hh.get_value(),
+                b_hy=self.b_hy.get_value(),
+                h0_tm1=self.h0_tm1.get_value())
 
-	def dump(self,path,filename='model.npz'):
-		with open(path + filename, "wb") as thefile:
-			np.savez(thefile, W_uh=self.W_uh.get_value(), W_hh=self.W_hh.get_value(), W_hy=self.W_hy.get_value(), b_hh=self.b_hh.get_value(), b_hy=self.b_hy.get_value(), h0_tm1=self.h0_tm1.get_value())
+    def importdump(self, path_to_file):
+        with open(path_to_file, "rb") as thefile:
+            npzfile = np.load(thefile)
+            self.W_uh.set_value(npzfile['W_uh'])
+            self.W_hh.set_value(npzfile['W_hh'])
+            self.W_hy.set_value(npzfile['W_hy'])
+            self.b_hh.set_value(npzfile['b_hh'])
+            self.b_hy.set_value(npzfile['b_hy'])
+            self.h0_tm1.set_value(npzfile['h0_tm1'])
 
-	def importdump(self,path_to_file):
-		with open(path_to_file, "rb") as thefile:
-			npzfile = np.load(thefile)
-			self.W_uh.set_value(npzfile['W_uh'])
-			self.W_hh.set_value(npzfile['W_hh'])
-			self.W_hy.set_value(npzfile['W_hy'])
-			self.b_hh.set_value(npzfile['b_hh'])
-			self.b_hy.set_value(npzfile['b_hy'])
-			self.h0_tm1.set_value(npzfile['h0_tm1'])
 
 if __name__ == '__main__':
-	rnn = RNN(2, 20, 1)
-	lr = 0.01
-	e = 1
-	vals = []
-	for i in xrange(int(5e5)):
-		u = np.random.rand(10,2)
-		t = np.dot(u[:,0], u[:,1])
-		c = rnn.train_step(u, t, lr)
-		print("iteration {0}: {1}".format(i, np.sqrt(c)))
-		e = 0.1*np.sqrt(c) + 0.9*e
-		if i % 1000 == 0:
-			vals.append(e)
-	plt.plot(vals)
-	plt.savefig('plots/error.png')
+    rnn = RNN(2, 20, 1)
+    lr = 0.01
+    e = 1
+    vals = []
+    for i in xrange(int(5e5)):
+        u = np.random.rand(10, 2)
+        t = np.dot(u[:, 0], u[:, 1])
+        c = rnn.train_step(u, t, lr)
+        print("iteration {0}: {1}".format(i, np.sqrt(c)))
+        e = 0.1 * np.sqrt(c) + 0.9 * e
+        if i % 1000 == 0:
+            vals.append(e)
+    plt.plot(vals)
+    plt.savefig('plots/error.png')
--- dragonfire/utilities.py
+++ dragonfire/utilities.py
@@ -12,74 +12,85 @@ from sys import stdout
 
 import realhud
 
-DRAGONFIRE_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+DRAGONFIRE_PATH = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
 FNULL = open(os.devnull, 'w')
 
 songRunning = False
+
+
 class TTA:
+    def __init__(self, args):
+        self.headless = args["headless"]
+        self.silent = args["silent"]
+        realhud.load_gif(DRAGONFIRE_PATH + "/realhud/animation/avatar.gif")
 
-	def __init__(self, args):
-		self.headless = args["headless"]
-		self.silent = args["silent"]
-		realhud.load_gif(DRAGONFIRE_PATH + "/realhud/animation/avatar.gif")
+    def define(self, com="", msg="", sp="False"):
+        self.command = com
+        self.message = msg
+        self.speak = sp
 
-	def define(self, com="", msg="", sp="False"):
-		self.command = com
-		self.message = msg
-		self.speak = sp
+    def execute(self, duration):
+        try:
+            subprocess.Popen(["notify-send", "Dragonfire", self.message])
+        except BaseException:
+            pass
+        if self.command != "":
+            time.sleep(duration)
+            try:
+                subprocess.Popen(self.command, stdout=FNULL, stderr=FNULL)
+            except BaseException:
+                pass
+        # if self.speak == True:
+        #   self.say(self.message)
+        # else:
 
-	def execute(self,duration):
-		try:
-			subprocess.Popen(["notify-send","Dragonfire", self.message])
-		except:
-			pass
-		if self.command != "":
-			time.sleep(duration)
-			try:
-				subprocess.Popen(self.command, stdout=FNULL, stderr=FNULL)
-			except:
-				pass
-		#if self.speak == True:
-		#	self.say(self.message)
-		#else:
+    def say(self, message, dynamic=False, end=False):
+        # if songRunning == True:
+        #   subprocess.Popen(["rhythmbox-client","--pause"])
+        if len(message) < 10000:
+            if dynamic:
+                if end:
+                    print(message.upper())
+                    print(
+                        "_______________________________________________________________\n"
+                    )
+                else:
+                    print("Dragonfire: " + message.upper(), end=' ')
+                    stdout.flush()
+            else:
+                print("Dragonfire: " + message.upper())
+                print(
+                    "_______________________________________________________________\n"
+                )
+        if not self.silent:
+            tts_proc = subprocess.Popen(
+                "flite -voice slt -f /dev/stdin",
+                stdin=subprocess.PIPE,
+                stdout=FNULL,
+                stderr=FNULL,
+                shell=True)
+            message = "".join([i if ord(i) < 128 else ' ' for i in message])
+            tts_proc.stdin.write(message)
+            tts_proc.stdin.close()
+            # print "TTS process started."
 
-	def say(self,message,dynamic=False,end=False):
-		#if songRunning == True:
-		#	subprocess.Popen(["rhythmbox-client","--pause"])
-		if len(message) < 10000:
-			if dynamic:
-				if end:
-					print(message.upper())
-					print("_______________________________________________________________\n")
-				else:
-					print("Dragonfire: " + message.upper(), end=' ')
-					stdout.flush()
-			else:
-				print("Dragonfire: " + message.upper())
-				print("_______________________________________________________________\n")
-		if not self.silent:
-			tts_proc = subprocess.Popen("flite -voice slt -f /dev/stdin", stdin=subprocess.PIPE, stdout=FNULL, stderr=FNULL, shell=True)
-			message = "".join([i if ord(i) < 128 else ' ' for i in message])
-			tts_proc.stdin.write(message)
-			tts_proc.stdin.close()
-			#print "TTS process started."
+        pool = Pool(processes=1)
+        if not self.headless:
+            pool.apply_async(realhud.play_gif, [0.5, True])
+            # print "Avatar process started."
 
-		pool = Pool(processes=1)
-		if not self.headless:
-			pool.apply_async(realhud.play_gif, [0.5, True])
-			#print "Avatar process started."
+        if not self.silent:
+            tts_proc.wait()
+        pool.terminate()
+        # if songRunning == True:
+        #   subprocess.Popen(["rhythmbox-client","--play"])
 
-		if not self.silent:
-			tts_proc.wait()
-		pool.terminate()
-		#if songRunning == True:
-		#	subprocess.Popen(["rhythmbox-client","--play"])
-
-	def espeak(self,message):
-		subprocess.Popen(["espeak","-v","en-uk-north",message])
+    def espeak(self, message):
+        subprocess.Popen(["espeak", "-v", "en-uk-north", message])
 
 
 if __name__ == "__main__":
-	userin = TTA()
-	userin.define([" "]," ")
-	userin.say("Hello world!")
+    userin = TTA()
+    userin.define([" "], " ")
+    userin.say("Hello world!")
