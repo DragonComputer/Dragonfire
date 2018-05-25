6d4ce4e6f77c1cba639965146a0ee9469a80f599
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2018-05-25 23:33:39 +0300

    Make the package Python 3 only

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3 :: Only',
     ],
 
     # What does your project relate to?
