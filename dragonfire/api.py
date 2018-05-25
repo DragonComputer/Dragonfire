f9f3daf4a5dab4e99553031cc3c36ebb4a4a1e30
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2018-05-25 08:07:20 +0300

    Add the other endpoints

--- dragonfire/api.py
+++ dragonfire/api.py
@@ -23,7 +23,49 @@ def tagger(text):
             'is_stop': token.is_stop
             }
         data[token.text] = parse
-    return json.dumps(data)
+    return json.dumps(data, indent=4)
+
+@hug.get('/dep')
+def dependency_parser(text):
+    data = collections.OrderedDict()
+    doc = nlp(text)
+    for chunk in doc.noun_chunks:
+        parse = {
+            'root_text': chunk.root.text,
+            'root_dep': chunk.root.dep_,
+            'root_head_text': chunk.root.head.text,
+            }
+        data[chunk.text] = parse
+    return json.dumps(data, indent=4)
+
+@hug.get('/ner')
+def entity_recognizer(text):
+    data = collections.OrderedDict()
+    doc = nlp(text)
+    for ent in doc.ents:
+        parse = {
+            'start_char': ent.start_char,
+            'end_char': ent.end_char,
+            'label': ent.label_,
+            }
+        data[ent.text] = parse
+    return json.dumps(data, indent=4)
+
+@hug.get('/token')
+def tokenizer(text):
+    data = []
+    doc = nlp(text)
+    for token in doc:
+        data.append(token.text)
+    return json.dumps(data, indent=4)
+
+@hug.get('/sent')
+def sentence_segmenter(text):
+    data = []
+    doc = nlp(text)
+    for sent in doc.sents:
+        data.append(sent.text)
+    return json.dumps(data, indent=4)
 
 
 class Run():
