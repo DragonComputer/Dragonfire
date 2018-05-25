4f4a7f3392f175064cb122654febac1148808f91
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2018-05-25 08:38:30 +0300

    Make the API more atomic

--- dragonfire/api.py
+++ dragonfire/api.py
@@ -9,6 +9,9 @@ import json
 nlp = None
 
 @hug.get('/tag')
+def tagger_end(text):
+    return json.dumps(tagger(text), indent=4)
+
 def tagger(text):
     data = collections.OrderedDict()
     doc = nlp(text)
@@ -23,9 +26,12 @@ def tagger(text):
             'is_stop': token.is_stop
             }
         data[token.text] = parse
-    return json.dumps(data, indent=4)
+    return data
 
 @hug.get('/dep')
+def dependency_parser_end(text):
+    return json.dumps(dependency_parser(text), indent=4)
+
 def dependency_parser(text):
     data = collections.OrderedDict()
     doc = nlp(text)
@@ -36,9 +42,12 @@ def dependency_parser(text):
             'root_head_text': chunk.root.head.text,
             }
         data[chunk.text] = parse
-    return json.dumps(data, indent=4)
+    return data
 
 @hug.get('/ner')
+def entity_recognizer_end(text):
+    return json.dumps(entity_recognizer(text), indent=4)
+
 def entity_recognizer(text):
     data = collections.OrderedDict()
     doc = nlp(text)
@@ -49,24 +58,44 @@ def entity_recognizer(text):
             'label': ent.label_,
             }
         data[ent.text] = parse
-    return json.dumps(data, indent=4)
+    return data
 
 @hug.get('/token')
+def tokenizer_end(text):
+    return json.dumps(tokenizer(text), indent=4)
+
 def tokenizer(text):
     data = []
     doc = nlp(text)
     for token in doc:
         data.append(token.text)
-    return json.dumps(data, indent=4)
+    return data
 
 @hug.get('/sent')
+def sentence_segmenter_end(text):
+    return json.dumps(sentence_segmenter(text), indent=4)
+
 def sentence_segmenter(text):
     data = []
     doc = nlp(text)
     for sent in doc.sents:
         data.append(sent.text)
-    return json.dumps(data, indent=4)
+    return data
 
+@hug.get('/cmd')
+def cmd(text):
+    return json.dumps(all_in_one(text), indent=4)
+
+def all_in_one(text):
+    data = []
+    sents = sentence_segmenter(text)
+    for sent in sents:
+        sent_data = {}
+        sent_data['tags'] = tagger(sent)
+        sent_data['deps'] = dependency_parser(sent)
+        sent_data['ners'] = entity_recognizer(sent)
+        data.append(sent_data)
+    return data
 
 class Run():
     def __init__(self, nlpRef):
