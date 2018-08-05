c32c5d3543a5b67a35e655b8dffeaf973d927de8
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2018-08-05 14:37:59 +0300

    Parametrize the tests for the top-level module of Dragonfire

--- dragonfire/tests/test_dragonfire.py
+++ dragonfire/tests/test_dragonfire.py
@@ -44,11 +44,67 @@ def virtual_assistant():
     return VirtualAssistant(args, userin, testing=True)
 
 
-def test_builtin_commands(virtual_assistant):
+@pytest.mark.parametrize("command, response", [
+    (76, False),
+    ("enough", "Dragonfire quiets."),
+    ("shut up", "Dragonfire quiets."),
+    ("What is your name?", "My name is Dragonfire."),
+    ("What is your gender?", "I have a female voice but I don't have a gender identity. I'm a computer program, " + user_prefix + "."),
+    ("Who am I?", "Your name is " + user_full_name + ", " + user_prefix + "."),
+    ("open Blender", "Blender 3D computer graphics software"),
+    ("run draw", "Opening LibreOffice Draw"),
+    ("open impress", "Opening LibreOffice Impress"),
+    ("open math", "Opening LibreOffice Math"),
+    ("open writer", "Opening LibreOffice Writer"),
+    ("open Gimp", "Opening the photo editor software."),
+    ("open Inkscape", "Opening the vectorial drawing software."),
+    ("open office suite", "Opening LibreOffice"),
+    ("run Blender", "Blender 3D computer graphics software"),
+    ("open Kdenlive", "Opening the video editor software."),
+    ("open browser", "Web browser"),
+    ("start Chrome", "Web browser"),
+    ("open Firefox", "Web browser"),
+    ("open Steam", "Opening Steam Game Store"),
+    ("open files", "File Manager"),
+    ("open file manager", "File Manager"),
+    ("open camera", "Camera"),
+    ("open calendar", "Calendar"),
+    ("open calculator", "Calculator"),
+    ("open software center", "Software Center"),
+    ("I'm a girl", "Pardon, my lady."),
+    ("call me master", "OK, master."),
+    ("I'm a boy", "Pardon, sir."),
+    ("keyboard blabla", "keyboard"),
+    ("type blabla", "keyboard"),
+    ("enter", "enter"),
+    ("new line", "enter"),
+    ("new tab", "new tab"),
+    ("switch tab", "switch tab"),
+    ("close", "close"),
+    ("back", "back"),
+    ("forward", "forward"),
+    ("swipe left", "swipe left"),
+    ("swipe right", "swipe right"),
+    ("swipe up", "swipe up"),
+    ("swipe down", "swipe down"),
+    ("play", "play"),
+    ("shut down the computer", "Shutting down"),
+    ("power off the computer", "Shutting down"),
+    ("goodbye", "Goodbye, " + user_prefix),
+    ("bye", "Goodbye, " + user_prefix),
+    ("see you later", "Goodbye, " + user_prefix),
+    ("search Katy Perry in YouTube", "Katy Perry - Roar (Official)"),
+    ("find Katy Perry on YouTube", "Katy Perry - Roar (Official)"),
+    ("search Albert Einstein on the internet", "Albert Einstein"),
+    ("search the images of Albert Einstein in Google images", "Albert Einstein")
+])
+def test_builtin_commands(virtual_assistant, command, response):
+    assert virtual_assistant.command("hey") in hey_answers
+    assert virtual_assistant.command(command) == response
 
-    assert not virtual_assistant.command(76)
+
+def test_builtin_commands_sleep(virtual_assistant):
     assert virtual_assistant.command("open files") == ""
-
     assert virtual_assistant.command("dragonfire") in hey_answers
     assert virtual_assistant.command("dragon fire") in hey_answers
     assert virtual_assistant.command("go to sleep") == "I'm going to sleep"
@@ -56,70 +112,15 @@ def test_builtin_commands(virtual_assistant):
     assert virtual_assistant.command("stop listening") == "I'm going to sleep"
     assert virtual_assistant.command("wake up") in hey_answers
 
-    assert virtual_assistant.command("enough") == "Dragonfire quiets."
-    assert virtual_assistant.command("shut up") == "Dragonfire quiets."
 
-    assert virtual_assistant.command("What is your name?") == "My name is Dragonfire."
-    assert virtual_assistant.command("What is your gender?") == "I have a female voice but I don't have a gender identity. I'm a computer program, " + user_prefix + "."
-    assert virtual_assistant.command("Who am I?") == "Your name is " + user_full_name + ", " + user_prefix + "."
-
-    assert virtual_assistant.command("open Blender") == "Blender 3D computer graphics software"
-    assert virtual_assistant.command("run draw") == "Opening LibreOffice Draw"
-    assert virtual_assistant.command("open impress") == "Opening LibreOffice Impress"
-    assert virtual_assistant.command("open math") == "Opening LibreOffice Math"
-    assert virtual_assistant.command("open writer") == "Opening LibreOffice Writer"
-    assert virtual_assistant.command("open Gimp") == "Opening the photo editor software."
-    assert virtual_assistant.command("open Inkscape") == "Opening the vectorial drawing software."
-    assert virtual_assistant.command("open office suite") == "Opening LibreOffice"
-    assert virtual_assistant.command("run Blender") == "Blender 3D computer graphics software"
-    assert virtual_assistant.command("open Kdenlive") == "Opening the video editor software."
-    assert virtual_assistant.command("open browser") == "Web browser"
-    assert virtual_assistant.command("start Chrome") == "Web browser"
-    assert virtual_assistant.command("open Firefox") == "Web browser"
-    assert virtual_assistant.command("open Steam") == "Opening Steam Game Store"
-    assert virtual_assistant.command("open files") == "File Manager"
-    assert virtual_assistant.command("open file manager") == "File Manager"
-    assert virtual_assistant.command("open camera") == "Camera"
-    assert virtual_assistant.command("open calendar") == "Calendar"
-    assert virtual_assistant.command("open calculator") == "Calculator"
-    assert virtual_assistant.command("open software center") == "Software Center"
-
-    assert virtual_assistant.command("I'm a girl") == "Pardon, my lady."
-    assert virtual_assistant.command("call me master") == "OK, master."
-    assert virtual_assistant.command("I'm a boy") == "Pardon, sir."
-
-    assert virtual_assistant.command("What's the temperature in New York?").startswith("The temperature in")
-
-    assert virtual_assistant.command("keyboard blabla") == "keyboard"
-    assert virtual_assistant.command("type blabla") == "keyboard"
-    assert virtual_assistant.command("enter") == "enter"
-    assert virtual_assistant.command("new line") == "enter"
-    assert virtual_assistant.command("new tab") == "new tab"
-    assert virtual_assistant.command("switch tab") == "switch tab"
-    assert virtual_assistant.command("close") == "close"
-    assert virtual_assistant.command("back") == "back"
-    assert virtual_assistant.command("forward") == "forward"
-    assert virtual_assistant.command("swipe left") == "swipe left"
-    assert virtual_assistant.command("swipe right") == "swipe right"
-    assert virtual_assistant.command("swipe up") == "swipe up"
-    assert virtual_assistant.command("swipe down") == "swipe down"
-    assert virtual_assistant.command("play") == "play"
-
-    assert virtual_assistant.command("shut down the computer") == "Shutting down"
-    assert virtual_assistant.command("power off the computer") == "Shutting down"
-
-    assert virtual_assistant.command("goodbye") == "Goodbye, " + user_prefix
-    assert virtual_assistant.command("bye") == "Goodbye, " + user_prefix
-    assert virtual_assistant.command("see you later") == "Goodbye, " + user_prefix
-
-    assert virtual_assistant.command("search Albert Einstein in Wikipedia").startswith("Albert Einstein ; 14 March 1879   18 April 1955) was a German-born theoretical physicist who developed the theory of relativity")
-    assert virtual_assistant.command("find Albert Einstein on Wikipedia").startswith("Albert Einstein ; 14 March 1879   18 April 1955) was a German-born theoretical physicist who developed the theory of relativity")
-
-    assert virtual_assistant.command("search Katy Perry in YouTube") == "Katy Perry - Roar (Official)"
-    assert virtual_assistant.command("find Katy Perry on YouTube") == "Katy Perry - Roar (Official)"
-
-    assert virtual_assistant.command("search Albert Einstein on the internet") == "Albert Einstein"
-    assert virtual_assistant.command("search the images of Albert Einstein in Google images") == "Albert Einstein"
+@pytest.mark.parametrize("command, response", [
+    ("What's the temperature in New York?", "The temperature in"),
+    ("search Albert Einstein in Wikipedia", "Albert Einstein ; 14 March 1879   18 April 1955) was a German-born theoretical physicist who developed the theory of relativity"),
+    ("find Albert Einstein on Wikipedia", "Albert Einstein ; 14 March 1879   18 April 1955) was a German-born theoretical physicist who developed the theory of relativity")
+])
+def test_builtin_commands_startswith(virtual_assistant, command, response):
+    assert virtual_assistant.command("hey") in hey_answers
+    assert virtual_assistant.command(command).startswith(response)
 
 
 def test_arithmetic_response(virtual_assistant):
