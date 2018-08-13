f5da25c9158cf9f7a36e8d07559a40a6846adc05
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2018-08-13 16:56:00 +0300

    Revert back the changes in setup.py related to automatic versioning

--- setup.py
+++ setup.py
@@ -54,9 +54,6 @@ def pkgconfig(*packages):
     return config
 
 
-# Dynamically calculate the version based on dragonfire.__version__
-version = __import__('dragonfire').__version__
-
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -67,7 +64,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=version,
+    version='1.0.0',
     description='Dragonfire is an open source virtual assistant project for Ubuntu based Linux distributions',
     long_description=long_description,
     long_description_content_type='text/markdown',
