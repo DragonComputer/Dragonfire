fc396765b63608571c22ea0f5d0e0fc66783538d
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2019-01-19 01:24:06 +0300

    Add libgtk2.0-0 and gir1.2-gtk-3.0 dependencies to install shell scripts

--- install-dev.sh
+++ install-dev.sh
@@ -16,7 +16,7 @@ NO_MODEL=$NO_MODEL
 apt-get update
 apt-get -y install debhelper python3 python3-all-dev libglib2.0-dev libcairo2-dev libgtk2.0-dev && \
 apt-get -y install dpkg python3-minimal ${misc:Pre-Depends} && \
-apt-get -y install ${python3:Depends} ${misc:Depends} flite python3-xlib portaudio19-dev python3-all-dev flac libnotify-bin python3-lxml python3-nltk python3-pyaudio python3-httplib2 python3-pip python3-setuptools python3-wheel libgstreamer1.0-dev gstreamer1.0-plugins-good gstreamer1.0-tools subversion libatlas-base-dev automake autoconf libtool && \
+apt-get -y install ${python3:Depends} ${misc:Depends} flite python3-xlib portaudio19-dev python3-all-dev flac libnotify-bin python3-lxml python3-nltk python3-pyaudio python3-httplib2 python3-pip python3-setuptools python3-wheel libgstreamer1.0-dev gstreamer1.0-plugins-good gstreamer1.0-tools subversion libatlas-base-dev automake autoconf libtool libgtk2.0-0 gir1.2-gtk-3.0 && \
 
 pip3 install -e .
 
